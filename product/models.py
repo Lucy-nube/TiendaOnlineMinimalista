0f92bad95d2177c0a8cd0979a1b428f17f15dce9
Lucy-nube <Lucy24072001@gmail.com>
2026-04-12 20:49:50 +0200

    correcting settings.py

--- product/models.py
+++ product/models.py
@@ -1,13 +1,15 @@
 from io import BytesIO
 from PIL import Image
-from django.core.files import File
+from django.core.files.base import ContentFile
 from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
+    
     class Meta:
         ordering = ('name',)
+        
     def __str__(self):
         return self.name
 
@@ -17,8 +19,8 @@ class Product(models.Model):
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -27,23 +29,40 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    # Sobreescribimos el método save para generar el thumbnail una sola vez
     def save(self, *args, **kwargs):
+        # Generar thumbnail solo si hay imagen y no existe el thumbnail aún
         if self.image and not self.thumbnail:
             self.thumbnail = self.make_thumbnail(self.image)
         super().save(*args, **kwargs)
 
     def get_image(self):
-        return self.image.url if self.image else ''
+        if self.image:
+            return self.image.url
+        return ''
 
     def get_thumbnail(self):
-        return self.thumbnail.url if self.thumbnail else (self.image.url if self.image else '')
+        if self.thumbnail:
+            return self.thumbnail.url
+        if self.image:
+            return self.image.url
+        return ''
 
     def make_thumbnail(self, image, size=(300, 200)):
+        # Abrimos la imagen original
         img = Image.open(image)
+        
+        # Convertimos a RGB para evitar errores con archivos PNG o nombres extraños
         if img.mode != 'RGB':
             img = img.convert('RGB')
+            
         img.thumbnail(size)
+        
+        # Guardamos el resultado en memoria
         thumb_io = BytesIO()
         img.save(thumb_io, 'JPEG', quality=85)
-        return File(thumb_io, name=image.name)
+        
+        # Creamos un archivo de contenido para Django
+        name = image.name.replace('uploads/', '') # Limpiamos el nombre
+        thumbnail = ContentFile(thumb_io.getvalue(), name=name)
+        
+        return thumbnail
