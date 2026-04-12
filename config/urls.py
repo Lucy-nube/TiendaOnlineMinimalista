cd8e2a1cf68180df618c0722a441aac96b772adb
Lucy-nube <Lucy24072001@gmail.com>
2026-04-12 20:22:23 +0200

    correcting settings.py

--- config/urls.py
+++ config/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls.authtoken')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.MEDIA_URL and settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
